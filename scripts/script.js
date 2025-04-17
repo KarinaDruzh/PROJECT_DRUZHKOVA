edb370b3a6f357b1ded248c78765d9d3259cdafd
KarinaDruzh <druzhkova234@gmail.com>
2025-04-17 16:18:40 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,34 @@
 'use strict'
 document.addEventListener("DOMContentLoaded", () => {
-    console.log('Скрипт отработал корректно')
+    //* 1. Начало.
+    //* 2. Получаем все элементы кнопок 
+    //* 3. Для каждой кнопки (проверям есть ли такие кнопки):
+    //*    3.1. Добавляем обработчик клика на кнопку:
+    //*        3.1.1. Да:
+    //*          3.1.1.1. показываем текст при клике и убираем стандартное действие.
+    //*        3.1.2. Нет: продолжаем.
+    //*    3.2. 
+    //* 4. Конец.
+
+
+    const link = document.querySelector(".link-law");
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        console.log("Мышка кликнула на кнопку, показываем текст");
+    });
+
+    const link2 = document.querySelector(".link-agreement");
+    link2.addEventListener('click', (event) => {
+        event.preventDefault();
+        console.log("Мышка кликнула на кнопку, показываем текст");
+    });
+
+    const link3 = document.querySelector(".link-confidentiality");
+    link3.addEventListener('click', (event) => {
+        event.preventDefault();
+        console.log("Мышка кликнула на кнопку, показываем текст");
+    });
+
+    
+    
 });
